ff5470521c91dccd66e96194c0fbd954d7ee17db
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2021-12-06 22:19:27 +0100

    pre-make 1

---
+++ ide.cs
@@ -0,0 +1,10 @@
+public class IDE {
+    public IDE() {
+        super("IDE");
+    }
+    void load() {
+        this.IDE.loader.getFile("extensions/ext.json");
+    }
+    void loader<String>() {}
+    void getFile<String>(String file) {}
+}
